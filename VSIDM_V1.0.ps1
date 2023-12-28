a8463b0539ab8d13919f167cbdd8b35b0a7dfe48
failedtech <failedtech69@gmail.com>
2023-12-28 17:48:13 +0330

    12/28/2023 05:48 PM

--- VSIDM_V1.0.ps1
+++ VSIDM_V1.0.ps1
@@ -1,17 +1,39 @@
-TAdd-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Windows.Forms
 
-# Create FolderBrowserDialog
-$folderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog
-$folderBrowser.Description = "Select Visual Studio Offline Folder"
-$folderBrowser.ShowNewFolderButton = $true
-$folderBrowser.SelectedPath = Get-Location
+Function Get-FolderLocation {
+    param (
+        [string]$initialPath = (Get-Location)
+    )
 
-# Definitions
-$foldersTable = @()
-$packagesTable = @()
-$diffTable = @()
-$idRegex = '(.+?)(?:,|$)'
-$versionRegex = 'version=(.+?)(?:,|$)'
-$chipRegex = 'chip=(.+?)(?:,|$)'
-$languageRegex = 'language=(.+?)(?:,|$)'
+    $folderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog
+    $folderBrowser.Description = "Select Visual Studio Offline Folder"
+    $folderBrowser.ShowNewFolderButton = $true
+    $folderBrowser.SelectedPath = $initialPath
 
+    $result = [PSCustomObject]@{
+        IsValid = $folderBrowser.ShowDialog() -eq 'OK'
+        Value   = $folderBrowser.SelectedPath ?? "Folder selection canceled."
+    }
+
+    return $result
+}
+
+Function Test-CatalogPath {
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [PSCustomObject]$folderPath
+    )
+    return Test-Path (Join-Path $folderPath "Catalog.json") -PathType Leaf
+}
+
+Function Get-CatalogContent {
+    # Convert JSON content to PowerShell object
+    return Get-Content -Path ($catalogPath) -Raw | ConvertFrom-Json
+}
+
+
+"Catalog.json not found"
+$a = Get-FolderLocation
+$a.Value 
+$a.Value | Test-CatalogPath
+Pause
